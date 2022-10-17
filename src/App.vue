bd9486b59d45f543617bb164f2e62bd158d5e7d2
shideshan <307042873@qq.com>
2022-10-18 02:04:06 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <div class="bg-slate-800 rounded-xl p-8 md:flex md:p-0">
+    <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwww.milkimpression.cn%2Fbbs%2Fdata%2Fattachment%2Fforum%2F201303%2F03%2F115758zr5k58kb8r3xk830.jpg&refer=http%3A%2F%2Fwww.milkimpression.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1668620875&t=01d499aa78e6f1a5e24bcc3ae43160c5" alt="" class="w-24 h-24 rounded-full mx-auto md:w-48 md:h-auto md:rounded-none">
+    <div class="pt-6 text-center space-y-4 md:p-8 md:text-left">
+      <p class="text-lg font-medium text-zinc-100">
+        asdha;osifnas'lfja案例库俄疯狂把控]]]李娇儿我都【艾尼斯佛啊【是否【哦啊还能违法【佛啊NSF从【】奥胖九年
+      </p>
+      <div class="font-medium">
+        <div class="text-sky-400">LGD_Sunday</div>
+        <div class="text-slate-500">爱丽丝回答楼上但是</div>
+      </div>
+    </div>
+  </div>
+</template>
