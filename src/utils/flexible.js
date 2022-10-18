b41b13995b8cf4ae98b914d787c08ec49173957d
shideshan <307042873@qq.com>
2022-10-18 19:20:27 +0800

    feat: 获取categories数据、动态设置rem、修改tailwind预设的fontsize

--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -11,3 +11,22 @@ const { width } = useWindowSize()
 export const isMobileTerminal = computed(() => {
   return width.value < PC_DEVICE_WIDTH
 })
+
+/**
+ * 动态设置rem值：根据用户的屏幕宽度进行计算，把计算出来的值设为html根标签的fontsize大小
+ */
+export const useREM = () => {
+  // 定义最大的 fontsize
+  const MAX_FONT_SIZE = 40
+  // 监听 html 解析完成事件
+  document.addEventListener('DOMContentLoaded', () => {
+    // 拿到 html 标签
+    const html = document.querySelector('html')
+    // 计算 fontsize：按照 屏幕宽度/10 来计算
+    let fontSize = window.innerWidth / 10
+    fontSize = fontSize > MAX_FONT_SIZE ? MAX_FONT_SIZE : fontSize
+
+    // 赋值给 html
+    html.style.fontSize = fontSize + 'px'
+  })
+}
