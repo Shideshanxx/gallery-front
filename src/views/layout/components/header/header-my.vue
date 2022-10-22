9f46f87fb9a54a0bd98106c966f2adbdccb8458d
shideshan <307042873@qq.com>
2022-10-22 22:32:53 +0800

    feat: header栏个人中心

--- src/views/layout/components/header/header-my.vue
+++ src/views/layout/components/header/header-my.vue
@@ -1,7 +1,67 @@
 <template>
-  <div>my</div>
+  <m-popover class="flex items-center" position="bottom-left">
+    <template #reference>
+      <div
+        class="relative flex items-center p-0.5 rounded-sm cursor-pointer duration-200 outline-none hover:bg-zinc-100"
+      >
+        <!-- 头像 -->
+        <img class="w-3 h-3 rounded-sm" src="@/assets/vue.svg" alt="" />
+        <!-- 下箭头 -->
+        <m-svg-icon
+          class="h-1.5 w-1.5 ml-0.5"
+          name="down-arrow"
+          fillClass="fill-zinc-900"
+        ></m-svg-icon>
+        <!-- vip -->
+        <m-svg-icon
+          class="h-1.5 w-1.5 absolute right-[16px] bottom-0"
+          name="vip"
+        ></m-svg-icon>
+      </div>
+    </template>
+    <div class="w-[140px] overflow-hidden">
+      <div
+        class="flex items-center p-1 cursor-pointer rounded hover:bg-zinc-100/60 dark:hover:bg-zinc-800"
+        v-for="item in menuArr"
+        :key="item.id"
+        @click="onItemClick(item.path)"
+      >
+        <m-svg-icon
+          :name="item.icon"
+          class="w-1.5 h-1.5 mr-1"
+          fillClass="fill-zinc-900 dark:fill-zinc-300"
+        ></m-svg-icon>
+        <span class="text-zinc-800 dark:text-zinc-300 text-sm">{{
+          item.title
+        }}</span>
+      </div>
+    </div>
+  </m-popover>
 </template>
 
-<script setup></script>
+<script setup>
+const menuArr = [
+  {
+    id: 0,
+    title: '个人资料',
+    icon: 'profile',
+    path: '/profile'
+  },
+  {
+    id: 1,
+    title: '升级 VIP',
+    icon: 'vip-profile',
+    path: '/member'
+  },
+  {
+    id: 2,
+    title: '退出登录',
+    icon: 'logout',
+    path: ''
+  }
+]
+
+const onItemClick = (path) => {}
+</script>
 
 <style lang="scss" scoped></style>
